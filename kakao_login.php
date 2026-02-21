113c8e577c7f59dc19ac012ce1f0330a99886c37
leejujin <lokia@naver.com>
2026-02-21 13:43:26 +0900

    update

--- kakao_login.php
+++ kakao_login.php
@@ -6,7 +6,9 @@ session_start();
 const KAKAO_REST_API_KEY   = 'd6cf1b953dfb5b853674b0c265090b1b';
 const KAKAO_CLIENT_SECRET = 'YqcjxkwRyqjK813eckdVyn4eAP87q4U7'; // 콘솔에서 Client Secret 사용 ON일 때만
 // const KAKAO_REDIRECT_URI  = 'https://cashhome.bizstore.co.kr/kakao_callback.php';
-const KAKAO_REDIRECT_URI  = 'https://cashhome.bizstore.co.kr/kakao_callback.php';
+;
+
+const KAKAO_REDIRECT_URI = 'https://cashhome.bizstore.co.kr/kakao_callback.php';
 
 $state = bin2hex(random_bytes(16));
 $_SESSION['kakao_oauth_state'] = $state;
