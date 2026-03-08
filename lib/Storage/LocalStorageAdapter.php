313f5852b61b8c17230145afaf5201bf26fd3a95
leejujin <lokia@naver.com>
2026-03-08 21:14:09 +0900

    update

--- lib/Storage/LocalStorageAdapter.php
+++ lib/Storage/LocalStorageAdapter.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+namespace Cashhome\Storage;
+
 require_once __DIR__ . '/StorageAdapterInterface.php';
 
 final class LocalStorageAdapter implements StorageAdapterInterface
